5ba6c1af30c4a1453665971638a4ee505cdbfc25
Srivarshini <srivarshini0402@gmail.com>
2025-07-22 23:29:19 +0530

    day6

---
+++ day6.py
@@ -0,0 +1,47 @@
+# Day6
+# Hurdle challenges - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     while wall_on_right()==True:
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear()==True:
+#         move()
+#     turn_left()
+
+# while at_goal()==False:
+#     if front_is_clear()==True:
+#         move()
+#     else:
+#         jump()
+
+    
+# All the functions not defined here are in-built in the Reeborg's World
+# Day6 Project - Reeborg's World - Maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+        
+
+    
